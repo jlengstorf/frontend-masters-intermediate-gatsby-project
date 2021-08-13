0ec698b68bc8a1e17a5125a1e53de6ee22b1ca02
Jason Lengstorf <jason@lengstorf.com>
2021-08-13 10:53:44 -0500

    add custom data types

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,3 +1,38 @@
+const authors = require('./src/data/authors.json');
+const books = require('./src/data/books.json');
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode } = actions;
+
+  authors.forEach((author) => {
+    createNode({
+      ...author,
+      id: createNodeId(`author-${author.slug}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Author',
+        content: JSON.stringify(author),
+        contentDigest: createContentDigest(author),
+      },
+    });
+  });
+
+  books.forEach((book) => {
+    createNode({
+      ...book,
+      id: createNodeId(`book-${book.isbn}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Book',
+        content: JSON.stringify(book),
+        contentDigest: createContentDigest(book),
+      },
+    });
+  });
+};
+
 exports.createPages = ({ actions }) => {
   const { createPage } = actions;
 

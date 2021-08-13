b3085d090146cef501738c0b15283b5a038c5e0b
Jason Lengstorf <jason@lengstorf.com>
2021-08-13 00:12:51 -0500

    wrap navigate in a useEffect

--- site/src/pages/account/[...].js
+++ site/src/pages/account/[...].js
@@ -1,7 +1,10 @@
+import * as React from 'react';
 import { navigate } from 'gatsby';
 
 export default function RedirectToAccountDashboard() {
-  navigate('/account/dashboard', { replace: true });
+  React.useEffect(() => {
+    navigate('/account/dashboard', { replace: true });
+  }, []);
 
   return null;
 }

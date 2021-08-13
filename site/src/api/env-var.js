66ef7e3d9a06040f77ebc44858c9d806413e3984
Jason Lengstorf <jason@lengstorf.com>
2021-08-13 15:48:09 -0500

    add INSECURE env var demo

---
+++ site/src/api/env-var.js
@@ -0,0 +1,6 @@
+export default function handler(_req, res) {
+  res.json({
+    GATSBY_PUBLIC_VALUE: process.env.GATSBY_PUBLIC_VALUE,
+    SECRET_VALUE: process.env.SECRET_VALUE,
+  });
+}
---
+++ site/src/pages/env-vars.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default function EnvVarsPage() {
+  return (
+    <>
+      <h1>Environment Variables</h1>
+      <ul>
+        <li>GATSBY_PUBLIC_VALUE: {process.env.GATSBY_PUBLIC_VALUE}</li>
+        <li>SECRET_VALUE: {process.env.SECRET_VALUE}</li>
+      </ul>
+    </>
+  );
+}

f83ab1d62b2a3381b8708ddac1787e80a286a74e
Jason Lengstorf <jason@lengstorf.com>
2021-08-12 23:49:16 -0500

    override theme config

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,4 +1,21 @@
 module.exports = {
+  siteMetadata: {
+    title: 'My Book Club',
+    navItems: [
+      {
+        label: 'Books',
+        path: '/books',
+      },
+      {
+        label: 'Authors',
+        path: '/authors',
+      },
+      {
+        label: 'Account',
+        path: '/account',
+      },
+    ],
+  },
   plugins: [
     'gatsby-theme-shared-nav',
     'gatsby-plugin-image',
